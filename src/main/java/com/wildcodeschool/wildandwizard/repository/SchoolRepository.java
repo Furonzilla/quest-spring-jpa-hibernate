500d286ba723b9e0b01f912ed54de87a020ef630
Ario Ngu <daitenshifuron@gmail.com>
2022-01-03 11:02:57 +0100

    quest done

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,9 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-// TODO : update this interface
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SchoolRepository {
+import com.wildcodeschool.wildandwizard.entity.School;
+import com.wildcodeschool.wildandwizard.entity.Wizard;
+
+public interface SchoolRepository extends JpaRepository<School, Long> {
 }
